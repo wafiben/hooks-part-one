aeac8b6b11c39849baad121db7e266a3e0fa5ad6
wafiben <wafibenjeddou@gmail.com>
2022-02-06 14:55:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState ,useEffect} from "react";
+import "./App.css";
+import { Button, Spinner } from "react-bootstrap";
 
 function App() {
+  const [count, setCount] = useState(1);
+  const [show, setShow] = useState(true);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    count > 0 && setCount(count - 1);
+  };
+  //---mounting
+  /*  useEffect(() => {
+    console.log("hello");
+  }, []); */
+  //----updating
+  /*    useEffect(()=>{
+  console.log('hello')
+  },[count]) */
+  const handleShow = () => {
+    show ? setShow(false) : setShow(true);
+  };
+  const content = <h1>this could be any tag</h1>;
+  const spinner = (
+    <Spinner animation="border" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </Spinner>
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={increment}>+</button>
+      <p>{count}</p>
+      <button onClick={decrement}>-</button>
+      <div>
+        <Button variant="primary" onClick={handleShow}>
+          Show
+        </Button>
+        <div>{show ? content : spinner}</div>
+      </div>
     </div>
   );
 }
